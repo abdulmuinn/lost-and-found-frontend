b21ff86248f6a4b067c00274aafdffe91c49eaf8
abdulmuinn <muinabdul000@gmail.com>
2025-07-01 14:01:53 +0700

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // URL dasar dari server backend Anda
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'lost-found-backend-production.up.railway.app/api';
 
 // Membuat instance axios
 const api = axios.create({
